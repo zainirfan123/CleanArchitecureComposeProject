08efdcf233e5719531c824575035a5c75a8f09be
Zain <zainirfan614@gmail.com>
2024-05-15 00:48:33 +0200

    added assets (Strings, drawables, app icons, fonts) Create custom font class for whole app.

---
+++ app/src/main/java/com/example/mindsvalleyapplication/feature_channels/presentation/helper/AppsFontUtils.kt
@@ -0,0 +1,13 @@
+package com.example.mindsvalleyapplication.feature_channels.presentation.helper
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.mindsvalleyapplication.R
+
+object AppsFontUtils {
+    val fontBold = FontFamily(Font(R.font.gilroy_bold, FontWeight.Bold))
+    val fontRegular = FontFamily(Font(R.font.gilroy_regular, FontWeight.Normal))
+    val fontLight = FontFamily(Font(R.font.gilroy_regular, FontWeight.Light))
+    val fontMedium = FontFamily(Font(R.font.gilroy_regular, FontWeight.Medium))
+}
