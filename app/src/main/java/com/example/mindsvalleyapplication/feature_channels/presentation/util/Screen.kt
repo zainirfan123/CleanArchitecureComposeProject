4a69551b8a0bb373a821925306215758f4b13dd2
Zain <zainirfan614@gmail.com>
2024-05-18 16:10:16 +0200

    screen class make data Object

--- app/src/main/java/com/example/mindsvalleyapplication/feature_channels/presentation/util/Screen.kt
+++ app/src/main/java/com/example/mindsvalleyapplication/feature_channels/presentation/util/Screen.kt
@@ -1,5 +1,5 @@
 package com.example.mindsvalleyapplication.feature_channels.presentation.util
 
 sealed class Screen(val route:String){
-    object Channels: Screen("channels_screen")
+    data object Channels: Screen("channels_screen")
 }
